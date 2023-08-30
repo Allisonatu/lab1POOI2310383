90f8510dbad64174486906c50f9539c07c8e9397
Allisonatu <143566354+Allisonatu@users.noreply.github.com>
2023-08-30 15:19:32 -0500

    Add files via upload

---
+++ ejercicio4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    int numA;
+	    int numB;
+	    
+	    Scanner leer = new Scanner(System.in);
+	    System.out.println("Ingrese un numA: ");
+	    numA=leer.nextInt();
+	    System.out.println("Ingrese un numB: ");
+	    numB=leer.nextInt();
+	    if ((numA>0 && numB>0)||(numA<0 && numB<0)){
+	        System.out.println("true");
+	    }
+	}
+}
